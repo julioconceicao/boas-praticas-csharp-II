5b8f737548ee42be908674b9b19e5c326b911170
Julio César G. Conceição <juliocesar.conceicao@novahaus.com.br>
2022-08-13 23:54:18 -0300

    build: create the programs folder

---
+++ boas-praticas-II-Alura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace boas_praticas_II_Alura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
