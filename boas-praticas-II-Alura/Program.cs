1058da8159ddaaf33b96670b3700e6f84c388f1a
Julio César G. Conceição <juliocesar.conceicao@novahaus.com.br>
2022-08-15 14:20:31 -0300

    feat: database connection - finish cap 1

--- boas-praticas-II-Alura/Program.cs
+++ boas-praticas-II-Alura/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using boas_praticas_II_Alura.src.CAP1;
 
 namespace boas_praticas_II_Alura
 {
@@ -6,7 +9,12 @@ namespace boas_praticas_II_Alura
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Chama a conexão com o Database através da classe ConnectionFactory
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+            
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "select * from tabela ";
+
         }
     }
 }
